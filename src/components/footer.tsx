49b62427a79d7a7052bdc1496d6a9bd16ccd909a
Bao-Huy Trinh <anthonytrinh0508@gmail.com>
2025-07-16 05:35:37 -0400

    react icons + _blank

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
+import {FaLinkedin, FaGithub} from 'react-icons/fa';
 
 function Footer() {
   return (
-    <footer className='flex gap-5 p-4 justify-center'>
-        Footer
+    <footer className='flex gap-3 p-4 justify-center'>
+        <a
+        href="https://www.linkedin.com/in/baohuytrinh/"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+            <FaLinkedin size={28} className='hover:text-blue-500 transition-colors' />
+        </a>
+        <a
+        href="https://github.com/baohuytrinh"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+            <FaGithub size={28} className="hover:text-gray-400 transition-colors" />
+        </a>
     </footer>
   )
 }
