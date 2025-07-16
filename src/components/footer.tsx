a991033292b56853e5b2f1ce4b9d8ceba52bb8ff
Bao-Huy Trinh <anthonytrinh0508@gmail.com>
2025-07-16 04:48:34 -0400

    navbar+footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
 
 function Footer() {
   return (
-    <div>F</div>
+    <footer className='flex gap-5 p-4 justify-center'>
+        Footer
+    </footer>
   )
 }
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+
 
 function NavBar() {
   return (
-    <div>navbar</div>
+    <nav className='flex gap-6 p-4 justify-center bg-gray-900 text-blue'>
+        <Link href={"/"} className='hover:underline' >Home</Link>
+        <Link href={"/Projects"} className='hover:underline' >Projects</Link>
+        <Link href={"/Contact"} className='hover:underline' >Contact</Link>
+    </nav>
   )
 }
 
