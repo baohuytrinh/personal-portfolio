9df774699298f3b11cb61351a7948a16d448fcc9
Bao-Huy Trinh <anthonytrinh0508@gmail.com>
2025-07-16 04:15:50 -0400

    pages and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,10 @@
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <header className='font-sans text-6xl top-1'>
+        Bao-Huy 
+        <h1 className='font-sans text-xl top-1'>welcome to my portfolio</h1>
+      </header>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         test
       </main>
